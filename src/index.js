ad48354e074b904bc418d725d8ffa2d00a40ed86
Vano Khuroshvili <vano.khuroshvili@gmail.com>
2020-12-22 02:44:39 +0400

    Clean up and reorganised code. Implemented ESLint with latest and most popular rules(airbnb config) and fix all errors and warnings that comes in every presented JS file Fixed and implemented tests running on NodeJS side Added and configured all required NPM commands to manage all steps - clean, lint, build, test

--- src/index.js
+++ src/index.js
@@ -1,73 +1,71 @@
-import Config from './config.js'
-import Storage from './storage.js'
+import Config from './config';
+import Storage from './storage';
 
-// Start the TonicPow service
-let TonicPow = {}
+// Start the TonicPow service and load modules
+const TonicPow = {
+  Config,
+  Storage,
+};
 
 // Load modules
-TonicPow.Config = Config
-TonicPow.Storage = Storage
+// TonicPow.Config = Config;
+// TonicPow.Storage = Storage;
 
 // setOreo for creating new oreos
 TonicPow.setOreo = (name, value, days) => {
-  let d = new Date()
-  d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days)
-  document.cookie = name + '=' + value + ';path=/;expires=' + d.toGMTString()
-}
+  const date = new Date();
+  date.setTime(date.getTime() + 24 * 60 * 60 * 1000 * days);
+  document.cookie = `${name}=${value};path=/;expires=${date.toUTCString()}`;
+};
 
 // captureVisitorSession will capture the session and store it
 // Builds a cookie so it's sent on requests automatically
 // Stores in local storage for easy access from the application
 TonicPow.captureVisitorSession = (customSessionId = '') => {
-  let sessionId = customSessionId
-  if ((!customSessionId || customSessionId.length === 0) && typeof window !== 'undefined') {
-    let urlParams = new URLSearchParams(window.location.search)
-    sessionId = urlParams.get(Config.sessionName)
+  let sessionId = customSessionId;
+
+  if ((!customSessionId || !customSessionId.length) && typeof window !== 'undefined') {
+    const urlParams = new URLSearchParams(window.location.search);
+    sessionId = urlParams.get(Config.sessionName);
   }
   if (sessionId && sessionId.length > 0) {
-    TonicPow.setOreo(Config.sessionName, sessionId, Config.maxSessionDays)
-    TonicPow.Storage.setStorage(Config.sessionName, sessionId, (24 * 60 * 60 * Config.maxSessionDays))
+    TonicPow.setOreo(Config.sessionName, sessionId, Config.maxSessionDays);
+    TonicPow.Storage.setStorage(Config.sessionName, sessionId, (24 * 60 * 60 * Config.maxSessionDays));
   }
-}
+};
 
 // getVisitorSession will get the session if it exists
-TonicPow.getVisitorSession = () => {
-  return TonicPow.Storage.getStorage(Config.sessionName)
-}
+TonicPow.getVisitorSession = () => TonicPow.Storage.getStorage(Config.sessionName);
 
 // loadDivs replaces each TonicPow div with a corresponding embed widget
 TonicPow.loadDivs = async () => {
-  // Get sticker address from parent page
-  // let stickerAddress = (document.head.querySelector('[name=bitcoin-address][content]')) ? document.head.querySelector('[name=bitcoin-address][content]').content : ''
-
   // Get all divs
-  let tonicDivs = document.getElementsByClassName(Config.widgetDivClass)
+  const tonicDivs = document.getElementsByClassName(Config.widgetDivClass);
 
   // Loop all divs that we found
   for (let i = tonicDivs.length - 1; i >= 0; i--) {
-
     // Set the div
-    let tonicDiv = tonicDivs[i]
+    const tonicDiv = tonicDivs[i];
 
     // Get the widget id
-    let widgetId = tonicDiv.getAttribute(Config.widgetId)
+    const widgetId = tonicDiv.getAttribute(Config.widgetId);
     if (!widgetId) {
-      console.log(widgetId + ' not found')
-      continue
+      console.log(`${widgetId} not found`);
+      continue;
     }
 
     // Get the custom environment (switching away from default: production)
-    let customEnvironment = tonicDiv.getAttribute(Config.customEnvironment)
-    Config.setEnvironment(customEnvironment)
+    const customEnvironment = tonicDiv.getAttribute(Config.customEnvironment);
+    Config.setEnvironment(customEnvironment);
 
     // Fire the request to load the widget data
-    let promise = await fetch(
-      Config.apiUrl + '/v1/widgets/display/' + widgetId + '?provider=embed-' + Config.version
-    )
-    let response = await promise.json()
+    const promise = await fetch(
+      `${Config.apiUrl}/v1/widgets/display/${widgetId}?provider=embed-${Config.version}`,
+    );
+    const response = await promise.json();
 
     // Set URI encoded title
-    let campaignTitle = encodeURIComponent(response.title)
+    const campaignTitle = encodeURIComponent(response.title);
 
     // Set the HTML
     tonicDiv.innerHTML = `
@@ -76,46 +74,46 @@ TonicPow.loadDivs = async () => {
       width="${response.width}" 
       height="${response.height}" 
       alt="${response.title}" />
-    </a>`
+    </a>`;
 
     // Set additional information
-    tonicDiv.setAttribute('data-width', response.width)
-    tonicDiv.setAttribute('data-height', response.height)
+    tonicDiv.setAttribute('data-width', response.width);
+    tonicDiv.setAttribute('data-height', response.height);
   }
-}
+};
 
 // Load the TonicPow script(s) and default settings
 TonicPow.load = () => {
   // We only work in a browser
   if (typeof window === 'undefined') {
-    console.error('TonicPow embed only works in the browser')
-    return
+    console.error('TonicPow embed only works in the browser');
+    return;
   }
 
   // Load all tonics found on the page (if we have div)
-  let tonicDivs = document.getElementsByClassName(Config.widgetDivClass)
+  const tonicDivs = document.getElementsByClassName(Config.widgetDivClass);
   if (tonicDivs && tonicDivs.length > 0) {
-    TonicPow.loadDivs().then(r => {
-      console.log('TonicPow widget(s) loaded!')
-    })
+    TonicPow.loadDivs().then(() => {
+      console.log('TonicPow widget(s) loaded!');
+    });
   }
 
   // Process visitor token
-  TonicPow.captureVisitorSession()
-}
+  TonicPow.captureVisitorSession();
+};
 
 // Load the TonicPow widget
 if (document.readyState === 'complete' || document.readyState === 'interactive') {
   // This loads if the <script> is dynamically injected into the page
-  TonicPow.load()
+  TonicPow.load();
 } else {
   // This loads if the <script> is hardcoded in the html page in the <head>
-  document.addEventListener('DOMContentLoaded', function () {
-    TonicPow.load()
-  })
+  document.addEventListener('DOMContentLoaded', () => {
+    TonicPow.load();
+  });
 }
 
 // Store on the window (safely)
 if (typeof window !== 'undefined') {
-  window.TonicPow = TonicPow
+  window.TonicPow = TonicPow;
 }
