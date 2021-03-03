ce6e0e54ca2b7dded266ded82819afe9e556f055
mrz1836 <mrz1818@gmail.com>
2021-03-03 15:55:01 -0500

    Build two targets, tonicpow.js and widget.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
-module.exports = {
+// Common configuration
+let config = {
+  mode: 'production',
+  target: 'web'
+};
+
+// Build a version for widgets (deprecated)
+let widgetJs = Object.assign({}, config, {
   output: {
     filename: 'widget.js'
   },
-  mode: 'production',
-  target: 'web'
-}
+});
+
+// Build a version for tonicpow.js (new)
+let tonicpowJs = Object.assign({}, config,{
+  output: {
+    filename: 'tonicpow.js'
+  },
+});
+
+// Return array of configurations
+module.exports = [
+  widgetJs, tonicpowJs,
+];
