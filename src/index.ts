7bd4a6bbb5f6f4f387df8d22b57170735a7b999e
Luke <rohenaz@gmail.com>
2021-06-28 19:30:31 -0400

    remove inline svg

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ export default class TonicPow {
   constructor(options?: TPow.TonicPowOptions) {
     // Set namespaces
     this.config = new Config()
+
     this.storage = new Storage()
     this.widgets = new Map<string, TPow.Widget>()
     this.options = options
@@ -188,6 +189,7 @@ export default class TonicPow {
       const css = `
           * { clear: all }
           .tonicpow-icon { width: 1.5rem; height: 1.5rem; cursor: pointer; margin: auto; }
+          .tonicpow-icon > object { pointer-events: none; }
           .tonicpow-widget-share-button { border: none; }
           .tonicpow-widget-share-button > iframe { overflow: hidden; }
           .tonicpow-modal { font-family: Nunito, Arial; display: flex; align-items: center; padding: 0 1em; text-align: center; width: 100%; height: 100%; position: fixed; top: 0; left: 0; }
@@ -234,6 +236,14 @@ export default class TonicPow {
   }
 
   private static showPopup(options: TPow.PopupOptions) {
+    // Assets
+    const copyIcon = `${window.TonicPow.config.hostUrl}/images/icons/copyIcon.svg`
+    const facebookIcon = `${window.TonicPow.config.hostUrl}/images/icons/facebookIcon.svg`
+    const twitterIcon = `${window.TonicPow.config.hostUrl}/images/icons/twitterIcon.svg`
+    const twetchIcon = `${window.TonicPow.config.hostUrl}/images/icons/twetchIcon.svg`
+    const closeIcon = `${window.TonicPow.config.hostUrl}/images/icons/closeIcon.svg`
+    const tonicPowPlaceholder = `${window.TonicPow.config.hostUrl}/images/image_placeholder_tonicpow.png`
+
     const elem = document.createElement('div')
     elem.id = 'tonicpow-widget-popup'
     elem.classList.add('tonicpow-modal')
@@ -245,31 +255,48 @@ export default class TonicPow {
     const copyIconElement: HTMLElement = document.createElement('div')
     copyIconElement.classList.add('tonicpow-icon')
     copyIconElement.onclick = () => window.TonicPow.copyText(options.shortLink?.short_link_url)
-    copyIconElement.innerHTML = copyIcon
+
+    const copyIconObject = document.createElement('object')
+    copyIconObject.setAttribute('data', copyIcon)
+    copyIconObject.setAttribute('width', '24')
+    copyIconObject.setAttribute('height', '24')
+    copyIconElement.appendChild(copyIconObject)
 
     // FACEBOOK
     const facebookIconElement: HTMLElement = document.createElement('div')
     facebookIconElement.classList.add('tonicpow-icon')
-    facebookIconElement.style.color = 'rgb(66, 103, 178)'
     facebookIconElement.onclick = () =>
       window.TonicPow.shareOnPlatform(options.shortLink, Platform.Facebook)
-    facebookIconElement.innerHTML = facebookIcon
+
+    const facebookIconObject = document.createElement('object')
+    facebookIconObject.setAttribute('data', facebookIcon)
+    facebookIconObject.setAttribute('width', '24')
+    facebookIconObject.setAttribute('height', '24')
+    facebookIconElement.appendChild(facebookIconObject)
 
     // TWITTER
     const twitterIconElement: HTMLElement = document.createElement('div')
     twitterIconElement.classList.add('tonicpow-icon')
-    twitterIconElement.style.color = 'rgb(29, 161, 242)'
     twitterIconElement.onclick = () =>
       window.TonicPow.shareOnPlatform(options.shortLink, Platform.Twitter)
-    twitterIconElement.innerHTML = twitterIcon
+
+    const twitterIconObject = document.createElement('object')
+    twitterIconObject.setAttribute('data', twitterIcon)
+    twitterIconObject.setAttribute('width', '24')
+    twitterIconObject.setAttribute('height', '24')
+    twitterIconElement.appendChild(twitterIconObject)
 
     // TWETCH
     const twetchIconElement: HTMLElement = document.createElement('div')
     twetchIconElement.classList.add('tonicpow-icon')
-    twetchIconElement.style.color = 'rgb(8, 90, 246)'
     twetchIconElement.onclick = () =>
       window.TonicPow.shareOnPlatform(options.shortLink, Platform.Twetch)
-    twetchIconElement.innerHTML = twetchIcon
+
+    const twetchIconObject = document.createElement('object')
+    twetchIconObject.setAttribute('data', twetchIcon)
+    twetchIconObject.setAttribute('width', '24')
+    twetchIconObject.setAttribute('height', '24')
+    twetchIconElement.appendChild(twetchIconObject)
 
     modalGridElement.appendChild(twetchIconElement)
     modalGridElement.appendChild(twitterIconElement)
@@ -281,13 +308,17 @@ export default class TonicPow {
     modalBoxElement.innerHTML = `
       <h2>
         ${options.title}
-        <div class="tonicpow-modal__close" onclick="TonicPow.closePopup();">
-          <div class="tonicpow-icon">${closeIcon}</div>
+        <div class="tonicpow-modal__close tonicpow-icon" onclick="TonicPow.closePopup();">
+          <object data="${closeIcon}"></object>
         </div>
       </h2>
       <div class="tonicpow-modal__campaign">
-        <img class="tonicpow-modal__campaign_image" src="${options.shortLink?.image_url}" />
-        <div class="tonicpow-modal__campaign_title">${options.shortLink?.title}</div>
+        <img class="tonicpow-modal__campaign_image" src="${
+          options.shortLink?.image_url?.length ? options.shortLink?.image_url : tonicPowPlaceholder
+        }" />
+        <div class="tonicpow-modal__campaign_title">${
+          options.shortLink?.title.length ? options.shortLink.title : 'TonicPow Campaign'
+        }</div>
       </div>
       <p id="tonicpow__short-link-text">${options.shortLink?.short_link_url}</p>      
 
@@ -468,10 +499,3 @@ declare global {
 }
 
 window.TonicPow = tpow || {}
-
-// Assets
-const copyIcon = `<svg fill="none" x="0px" y="0px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" class="w-6 h-6 mr-1 text-gray-800"><path fill="currentColor" d="M8 4c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h2zm0 2H6v14h12V6h-2a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2zm2-2v2h4V4h-4z"></path></svg>`
-const facebookIcon = `<svg fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd"></path></svg>`
-const twitterIcon = `<svg fill="currentColor" viewBox="0 0 24 24"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path></svg>`
-const twetchIcon = `<svg focusable="false" viewBox="0 0 20 17" aria-hidden="true" width="24"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.1804 11.1469L10.2223 11.839C10.3483 13.6326 9.24308 15.2709 7.49438 15.9065C6.85086 16.1325 5.75967 16.1607 5.0462 15.963C4.76641 15.8782 4.23481 15.5958 3.85709 15.3416L3.1716 14.8755L2.41616 15.1156C1.99647 15.2427 1.43689 15.4545 1.18508 15.5958C0.947252 15.7229 0.73741 15.7935 0.73741 15.7511C0.73741 15.511 1.25502 14.6919 1.6887 14.2399C2.27626 13.6044 2.10839 13.5479 0.919274 13.9716C0.205805 14.2117 0.191816 14.2117 0.331711 13.9433C0.41565 13.8021 0.849325 13.3078 1.31098 12.8558C2.0944 12.0791 2.13637 11.9943 2.13637 11.3446C2.13637 10.3419 2.61202 8.25163 3.08766 7.10763C3.969 4.96088 5.8576 2.74351 7.74619 1.62777C10.4042 0.060073 13.9436 -0.335381 16.9234 0.582639C17.9166 0.893352 19.6234 1.68426 19.6234 1.82549C19.6234 1.86786 19.1057 1.92436 18.4762 1.93848C17.1612 1.96673 15.8462 2.33394 14.727 2.98361L13.9716 3.43556L14.8389 3.73215C16.07 4.15585 17.1752 5.13036 17.455 6.04838C17.5389 6.34497 17.5109 6.3591 16.7275 6.3591L15.9161 6.37322L16.6016 6.69806C17.413 7.10764 18.1544 7.79968 18.5182 8.50585C18.784 9.01429 19.1197 10.2995 19.0218 10.3984C18.9938 10.4408 18.7 10.356 18.3643 10.243C17.399 9.88994 17.2731 9.97468 17.8327 10.5679C18.8819 11.6412 19.2037 13.2372 18.7 14.7484L18.4622 15.4263L17.5389 14.5083C15.6503 12.6581 13.426 11.5565 10.8799 11.2317L10.1804 11.1469ZM6.34177 14.0879L5.59676 14.0879L4.86489 13.0186L4.12863 14.0879L3.388 14.0879L4.44855 12.5891L3.45374 11.1648L4.19437 11.1648L4.86489 12.164L5.52664 11.1648L6.27604 11.1648L5.28122 12.5847L6.34177 14.0879Z" fill="currentColor"></path></svg>`
-const closeIcon = `<svg fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>`
